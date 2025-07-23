f6d384a0ebec355457715bc020b63eaef67606b8
Anup sharma <132664325+Anup00007@users.noreply.github.com>
2025-07-23 06:02:02 +0530

    Update Dashboard.js

--- src/service/Dashboard.js
+++ src/service/Dashboard.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const fetchDashboardData = async () => {
   try {
-    const res = await api.get("/dashboard");
+    const res = await api.get("/admin/dashboard");
     return res;
   } catch (err) {
     console.error("Error fetching dashboard data:", err);
