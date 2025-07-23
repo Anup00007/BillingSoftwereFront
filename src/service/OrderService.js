9efa743416856030d8f6a96412d62f23fd533b72
Anup sharma <132664325+Anup00007@users.noreply.github.com>
2025-07-23 06:01:17 +0530

    Update OrderService.js

--- src/service/OrderService.js
+++ src/service/OrderService.js
@@ -1,13 +1,13 @@
 import api from "./api";
 
 export const latestOrders = async () => {
-  return await api.get("/orders/latest");
+  return await api.get("/admin/orders/latest");
 };
 
 export const createOrder = async (order) => {
-  return await api.post("/orders", order);
+  return await api.post("/admin/orders", order);
 };
 
 export const deleteOrder = async (id) => {
-  return await api.delete(`/orders/${id}`);
+  return await api.delete(`/admin/orders/${id}`);
 };
